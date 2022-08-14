070ca20837dd6d99f1efb6a841939ad4dd89b1af
dmadon <deanna.madon@gmail.com>
2022-08-14 16:19:05 -0500

    progress being made  with query promises

--- runTest.js
+++ runTest.js
@@ -1,47 +1,61 @@
-const PORT = process.env.PORT||3001;
+
 const db = require('./db/connection');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const {getEmployees, addEmployee} = require ('./utils/employeeInfo');
 
-const start = () => {
 
-    const sql = 
-    `SELECT 
-    E.emp_id AS 'ID',
-    E.emp_last_name AS 'Last Name',
-    E.emp_first_name AS 'First Name',   
-    roles.role_title AS 'Title',
-    departments.dept_name AS 'Department',
-    roles.role_salary AS 'Salary',
-    CONCAT(M.emp_first_name," ",M.emp_last_name) AS Manager
-
-    FROM employees E
+const testPromise = () => {
+    return new Promise((resolve,reject) => {
+        const sql = 
+        `SELECT 
+        E.emp_id AS 'ID',
+        E.emp_last_name AS 'Last Name',
+        E.emp_first_name AS 'First Name',   
+        roles.role_title AS 'Title',
+        departments.dept_name AS 'Department',
+        roles.role_salary AS 'Salary',
+        CONCAT(M.emp_first_name," ",M.emp_last_name) AS Manager
     
-    JOIN roles
-    ON roles.role_id = E.emp_role_id
+        FROM employees E
+        
+        JOIN roles
+        ON roles.role_id = E.emp_role_id
+        
+        JOIN departments
+        ON departments.dept_id = roles.role_dept_id
     
-    JOIN departments
-    ON departments.dept_id = roles.role_dept_id
-
-    LEFT JOIN employees M
-    ON M.emp_id = E.emp_manager_id
-
-    ORDER BY E.emp_last_name ASC
+        LEFT JOIN employees M
+        ON M.emp_id = E.emp_manager_id
     
-    `;
+        ORDER BY E.emp_last_name ASC
+        
+        `;
+        
     
+        db.query(sql,(err,rows) => {
+            if(err){
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Rows retrieved!',
+                data: rows
+            })
+            ;
+        });
 
-    db.query(sql,(err,rows) => {
-        if(err){
-            console.log(err);
-            return;
-        }
-        console.table(rows);
-    });
-  
-    }
-
+    })
     
+}
 
-start();
+
+testPromise()
+.then((response) => {
+    console.table(response.data);
+})
+.then(() => {
+    console.log('did it work?')
+})
+

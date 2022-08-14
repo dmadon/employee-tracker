89da595cb1acf7a70151f45731913348c4139a18
dmadon <deanna.madon@gmail.com>
2022-08-14 13:27:01 -0500

    successfully worked out the self join statement in employees table

--- runTest.js
+++ runTest.js
@@ -6,37 +6,30 @@ const {getEmployees, addEmployee} = require ('./utils/employeeInfo');
 
 const start = () => {
 
-    const sql = `
-    SELECT
-    CONCAT(e.emp_first_name," ",e.emp_last_name) AS Employee,
-    CONCAT(m.emp_first_name," ",m.emp_last_name) AS Manager
-    FROM
-    employees e
-    LEFT JOIN
-    employees m ON m.emp_id = e.emp_manager_id
+    const sql = 
+    `SELECT 
+    E.emp_id AS 'ID',
+    E.emp_last_name AS 'Last Name',
+    E.emp_first_name AS 'First Name',   
+    roles.role_title AS 'Title',
+    departments.dept_name AS 'Department',
+    roles.role_salary AS 'Salary',
+    CONCAT(M.emp_first_name," ",M.emp_last_name) AS Manager
+
+    FROM employees E
+    
+    JOIN roles
+    ON roles.role_id = E.emp_role_id
+    
+    JOIN departments
+    ON departments.dept_id = roles.role_dept_id
+
+    LEFT JOIN employees M
+    ON M.emp_id = E.emp_manager_id
+
+    ORDER BY E.emp_last_name ASC
     
     `;
-
-
-
-    
-    // const sql = 
-    // `SELECT 
-    // employees.emp_id AS 'ID',
-    // employees.emp_last_name AS 'Last Name',
-    // employees.emp_first_name AS 'First Name',    
-    // roles.role_title AS 'Title',
-    // departments.dept_name AS 'Department',
-    // roles.role_salary AS 'Salary',
-    // employees.emp_manager_id AS 'Manager'
-    // FROM employees
-    // JOIN roles
-    // ON roles.role_id = employees.emp_role_id
-    // JOIN departments
-    // ON departments.dept_id = roles.role_dept_id
-    // ORDER BY employees.emp_last_name ASC
-    
-    // `;
     
 
     db.query(sql,(err,rows) => {
--- utils/employeeInfo.js
+++ utils/employeeInfo.js
@@ -5,10 +5,30 @@ const inquirer = require('inquirer');
 
 const getEmployees = () => {
     console.table(`
-    ---------------------------------
-               ALL EMPLOYEES
-    ---------------------------------`);
-    const sql = `SELECT * FROM employees ORDER BY emp_last_name ASC`
+-----------------------------------------------------------------------------------------
+                                       ALL EMPLOYEES
+-----------------------------------------------------------------------------------------`);
+    const sql = `SELECT 
+    E.emp_id AS 'ID',
+    E.emp_last_name AS 'Last Name',
+    E.emp_first_name AS 'First Name',   
+    roles.role_title AS 'Title',
+    departments.dept_name AS 'Department',
+    roles.role_salary AS 'Salary',
+    CONCAT(M.emp_first_name," ",M.emp_last_name) AS Manager
+
+    FROM employees E
+    
+    JOIN roles
+    ON roles.role_id = E.emp_role_id
+    
+    JOIN departments
+    ON departments.dept_id = roles.role_dept_id
+
+    LEFT JOIN employees M
+    ON M.emp_id = E.emp_manager_id
+
+    ORDER BY E.emp_last_name ASC`
     db.query(sql,(err,rows) => {
         if(err){
             console.log(err);

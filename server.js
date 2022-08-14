a3beffa9de5b36878db61f7deea4bdbe40e92b76
dmadon <deanna.madon@gmail.com>
2022-08-13 21:45:41 -0500

    figured out how to store inquirer answers as database records and use database records as inquirer answer options

--- server.js
+++ server.js
@@ -4,99 +4,45 @@ const PORT = process.env.PORT||3001;
 const db = require('./db/connection');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
+const {getEmployees, addEmployee} = require ('./utils/employeeInfo');
 
 
-const getEmployees = () => {
-const sql = `SELECT * FROM employees`
-db.query(sql,(err,rows) => {
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.table(rows);
-});
+const chooseEmployee = () => {
+
+    db.query("SELECT * FROM employees ORDER BY emp_last_name ASC", (err,results) => {
+
+        if(err)throw err;
+
+        return inquirer
+            .prompt([
+            { 
+                type: 'rawlist',
+                name: 'chooseEmployee',
+                message: "Choose an employee",
+                choices: function(){
+                    let choiceArr = [];
+                    for(i=0; i<results.length;i++){
+                        choiceArr.push(results[i].emp_last_name+", "+results[i].emp_first_name);
+                    }
+                    return choiceArr;
+                }
+            },
+            
+            ])
+            .then((answers) => {
+                console.log('You chose '+answers.chooseEmployee+'!');
+            })
+    });
 }
 
 
-const promptUser = () => {
-    return inquirer
-        .prompt([
-           { 
-            type: 'input',
-            name: 'firstName',
-            message: "What is your employee's first name?",
-            validate: firstNameInput => {
-                if(firstNameInput){
-                    return true;
-                }
-                else{
-                    console.log('First name is required.');
-                    return false;
-                }
-            }
-           },
-           {
-            type: 'input',
-            name: 'lastName',
-            message: "What is your employee's last name?",
-            validate: lastNameInput => {
-                if(lastNameInput){
-                    return true;
-                }
-                else{
-                    console.log('Last name is required.');
-                    return false;
-                }
-            }
-           },
-           {
-            // THIS WILL EVENTUALLY NEED TO BE POPULATED FROM POSSIBLE VALUES IN THE DATABASE
-            type: 'input',
-            name: 'empRoleId',
-            message: "What is your employee's role id?",
-            validate: empRoleIDInput => {
-                if(empRoleIDInput){
-                    return true;
-                }
-                else{
-                    console.log('Role ID is required.');
-                    return false;
-                }
-            }
-           },
-           {
-            // EVENTUALLY, THIS WILL NEED TO BE POPULATED BY A LIST OF POSSIBLE CHOICES FROM THE DATABASE
-            type: 'input',
-            name: 'empManagerId',
-            message: "What is your employee's manager's ID?"
-           }   
-           
-        ])
-        .then((answers) => {
-            console.table(answers);
-            db.query("INSERT INTO employees SET ?",
-                {
-                emp_first_name: answers.firstName,
-                emp_last_name: answers.lastName,
-                emp_role_id: answers.empRoleId,
-                emp_manager_id: answers.emp_manager_id
-                },  
-                function(error){
-                if(error) throw error;
-                console.log('Employee added!');
-                getEmployees(); 
-                })
-                
-            }) 
-        
-};
+chooseEmployee();
 
 
-promptUser();
+
 
 
 
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`);
 });
-
---
+++ utils/employeeInfo.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const db = require('../db/connection');
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+
+const getEmployees = () => {
+    const sql = `SELECT * FROM employees`
+    db.query(sql,(err,rows) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.table(rows);
+    });
+    }
+
+
+const addEmployee = () => {
+    return inquirer
+        .prompt([
+           { 
+            type: 'input',
+            name: 'firstName',
+            message: "What is your employee's first name?",
+            validate: firstNameInput => {
+                if(firstNameInput){
+                    return true;
+                }
+                else{
+                    console.log('First name is required.');
+                    return false;
+                }
+            }
+           },
+           {
+            type: 'input',
+            name: 'lastName',
+            message: "What is your employee's last name?",
+            validate: lastNameInput => {
+                if(lastNameInput){
+                    return true;
+                }
+                else{
+                    console.log('Last name is required.');
+                    return false;
+                }
+            }
+           },
+           {
+            // THIS WILL EVENTUALLY NEED TO BE POPULATED FROM POSSIBLE VALUES IN THE DATABASE
+            type: 'input',
+            name: 'empRoleId',
+            message: "What is your employee's role id?",
+            validate: empRoleIDInput => {
+                if(empRoleIDInput){
+                    return true;
+                }
+                else{
+                    console.log('Role ID is required.');
+                    return false;
+                }
+            }
+           },
+           {
+            // EVENTUALLY, THIS WILL NEED TO BE POPULATED BY A LIST OF POSSIBLE CHOICES FROM THE DATABASE
+            type: 'input',
+            name: 'empManagerId',
+            message: "What is your employee's manager's ID?"
+           }   
+           
+        ])
+        .then((answers) => {
+            console.table(answers);
+            db.query("INSERT INTO employees SET ?",
+                {
+                emp_first_name: answers.firstName,
+                emp_last_name: answers.lastName,
+                emp_role_id: answers.empRoleId,
+                emp_manager_id: answers.emp_manager_id
+                },  
+                function(error){
+                if(error) throw error;
+                console.log('Employee added!');
+                getEmployees(); 
+                })
+                
+            }) 
+        
+};
+
+module.exports = {getEmployees, addEmployee};
